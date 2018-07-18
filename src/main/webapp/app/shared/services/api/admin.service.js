02aba411798ea2e1f4c33a888244cdf4d9af01eb
gyemin.id <ppmt2018!>
2018-07-18 14:36:52 +0900

    #6 api service 개발  - api추가 수정

--- src/main/webapp/app/shared/services/api/admin.service.js
+++ src/main/webapp/app/shared/services/api/admin.service.js
@@ -11,7 +11,52 @@
 
         var admin = Restangular.one('admin');
 
-        return {};
+        return {
+            getBoardList: getBoardList,
+            getBoard: getBoard,
+            createBoard: createBoard,
+            modifyBoard: modifyBoard,
+            deleteBoard: deleteBoard,
+            inactiveBoard: inactiveBoard,
+            writeReply: writeReply,
+            modifyReply: modifyReply,
+            deleteReply: deleteReply
+        };
 
+        function getBoardList(data) {
+            return admin.one('board').doPOST(data, 'list');
+        }
+
+        function getBoard(id) {
+            return admin.one('board').one('detail', id).doGET();
+        }
+
+        function createBoard(data) {
+            return admin.doPOST(data, 'board');
+        }
+
+        function modifyBoard(data) {
+            return admin.doPUT(data, 'board');
+        }
+
+        function deleteBoard(id) {
+            return admin.one('board', id).doDELETE();
+        }
+
+        function inactiveBoard(id) {
+            return admin.one('board').one('inactive', id).doDELETE();
+        }
+
+        function writeReply(data) {
+            return admin.one('board').doPOST(data, 'reply');
+        }
+
+        function modifyReply(data) {
+            return admin.one('board').doPUT(data, 'reply');
+        }
+
+        function deleteReply(id) {
+            return admin.one('board').one('reply', id).doDELETE();
+        }
     }
 })();
